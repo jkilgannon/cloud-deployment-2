d8679c92b8042fb49ca7496ae6dad3decdfb6830
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2018-09-18 03:51:05 -0400

    Update profile.py
    Ver 2

--- profile.py
+++ profile.py
@@ -14,21 +14,20 @@ pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-#node = request.RawPC("node")
 
-# Add a XenVM node to the request.
-node = request.XenVM("node")
-
-# Use Centos
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-
-# Request a public IP address
-node.routable_control_ip = "true"
-
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
-
-# Print the RSpec to the enclosing page.
-pc.printRequestRSpec(request)
+for x in range(1,4)
+     # Add a XenVM node to the request.
+     node = request.XenVM("node"+x)
+     # Use Centos
+     node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+     # Request private IP addresses for everyone.
+     iface = node.addInterface("if1")
+     iface.component_id = "eth1"
+     iface.addAddress(rspec.IPv4Address("192.168.1."+x, "255.255.255.0"))
+    # Request a public IP address for node1
+     if x ==1
+          node.routable_control_ip = "true"
+     # Install and execute a script that is contained in the repository.
+     node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+     # Print the RSpec to the enclosing page.
+     pc.printRequestRSpec(request)
